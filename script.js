940becd9df3191458ca6b5abb6d5025d2393523f
Aman Yadav <ay.work07@gmail.com>
2024-07-31 10:07:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,8 @@
-//*************************************************printing 1 to 100 numbers
-
-/*for (let i = 1; i <= 100; i++) {
-    console.log(" i =",i)
-    
 
 
 
 
 
 
-}*/
- //***************************************print all even numbers from 0 to 100
- /* for (let i=0 ; i < 101; i++) {
-    if (i%2==0) {
-      console.log("i =", i)
-      
-    } 
-    
-               
-    
-    
-  }*/
 
 
- //******************************** */ use of for-of loop
-/*let str = "Aman Yadav";
-
-let size = 0;
-
- for (let  i of  str ){
-  console.log("i=",i);
-
-  size++;
-  
- }
- console.log(size)*/
-
- /*
-
-
-
-
-
- 
-
- //              use of for-in loop
-
- 
- 
- 
- let product = {
-  name : "Aman",
-  class : 12,
-  ifPassed : true,
- };
-
- for (let key in  product){
-  console.log("key = ",  key, "    value =", product[key])
- }
-
- */
