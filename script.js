de1b30c73e0376dafff8e25bb0a68a9d53093b99
Aman Yadav <ay.work07@gmail.com>
2024-08-01 17:14:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+  
+/*let oldDiv = document.querySelector(".container");
+let newDiv = document.createElement("div");
+
+
+oldDiv.before(newDiv);
+newDiv.innerText = "This is a new div ";
+newDiv.remove();*/
+/*let newBtn = document.createElement("button");
+newBtn.innerText = "click me";
+newBtn.style.backgroundColor = "red";
+newBtn.style.color = "white";
+document.querySelector("body").prepend(newBtn);*/
+
+
+
+
+
+
 
 
 
